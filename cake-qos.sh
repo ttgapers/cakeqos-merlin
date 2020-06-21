8f21ec8df02697ad308e84a03213aeec22f74188
jackiechun <jackiechun@dyu.ca>
2020-06-21 01:55:37 -0400

    v0.0.3 release
    Add update and uninstall features, more flexibility in choosing prioritization queue (besteffort vs diffserv4; defaults to besteffort if none specified), establish foundation to support update feature (versions.txt).

--- cake-qos.sh
+++ cake-qos.sh
@@ -1,24 +1,82 @@
 #!/bin/sh
-# CAKE QoS v0.0.2 - port for Merlin firmware supported routers
-# https://www.snbforums.com/threads/rt-ac86u-i-built-cake.49190/
+# CakeQOS-Merlin v0.0.3 - port for Merlin firmware supported routers
 # Site: https://github.com/ttgapers/cakeqos-merlin
+# Thread: https://www.snbforums.com/threads/release-cakeqos-merlin.64800/
 # Credits: robcore, Odkrys, ttgapers, jackiechun
 
 readonly SCRIPT_NAME="cake-qos"
 
+### Cake Download
+cake_download() {
+	if [ "${1}" = "update" ]; then
+		VERSION_LOCAL_CAKE=$(opkg list_installed | grep "^sched-cake-oot - " | awk -F" - " '{print $2}' | cut -d- -f-4)
+		VERSION_LOCAL_TC=$(opkg list_installed | grep "^tc-adv - " | awk -F" - " '{print $2}')
+	elif [ "${1}" = "install" ]; then
+		VERSION_LOCAL_CAKE="0"
+		VERSION_LOCAL_TC="0"
+	fi
+	if [ "${2}" = "ac86u" ]; then
+		FILE1_TYPE="1"
+	elif [ "${2}" = "ax88u" ]; then
+		FILE1_TYPE="ax"
+	fi
+	VERSIONS_ONLINE=$(/usr/sbin/curl --retry 3 -s "https://raw.githubusercontent.com/ttgapers/cakeqos-merlin/master/versions.txt")
+	if [ "${VERSIONS_ONLINE}" != "" ] && [ "${VERSION_LOCAL_CAKE}" != "" ] && [ "${VERSION_LOCAL_TC}" != "" ]; then
+		VERSION_ONLINE_CAKE=$(echo "$VERSIONS_ONLINE" | awk -F"|" '{print $1}')
+		VERSION_ONLINE_TC=$(echo "$VERSIONS_ONLINE" | awk -F"|" '{print $2}')
+		VERSION_ONLINE_SUFFIX=$(echo "$VERSIONS_ONLINE" | awk -F"|" '{print $3}')
+		if [ "${VERSION_LOCAL_CAKE}" != "${VERSION_ONLINE_CAKE}" ] || [ "${VERSION_LOCAL_TC}" != "${VERSION_ONLINE_TC}" ]; then
+			echo "Updated binaries detected, updating..."
+			FILE1="sched-cake-oot_${VERSION_ONLINE_CAKE}-${FILE1_TYPE}_${VERSION_ONLINE_SUFFIX}.ipk"
+			FILE2="tc-adv_${VERSION_ONLINE_TC}_${VERSION_ONLINE_SUFFIX}.ipk"
+			FILE1_OUT="sched-cake-oot.ipk"
+			FILE2_OUT="tc-adv.ipk"
+			/usr/sbin/curl --retry 3 "https://raw.githubusercontent.com/ttgapers/cakeqos-merlin/master/${FILE1}" -o "/tmp/home/root/${FILE1_OUT}"
+			/usr/sbin/curl --retry 3 "https://raw.githubusercontent.com/ttgapers/cakeqos-merlin/master/${FILE2}" -o "/tmp/home/root/${FILE2_OUT}"
+			if [ -f "/tmp/home/root/${FILE1_OUT}" ] && [ -f "/tmp/home/root/${FILE2_OUT}" ]; then
+				if [ "${1}" = "update" ]; then
+					opkg --autoremove remove sched-cake-oot
+					opkg --autoremove remove tc-adv
+				fi
+				/opt/bin/opkg install "/tmp/home/root/${FILE1_OUT}"
+				/opt/bin/opkg install "/tmp/home/root/${FILE2_OUT}"
+				rm "/tmp/home/root/${FILE1_OUT}"
+				rm "/tmp/home/root/${FILE2_OUT}"
+				return 0
+			else
+				echo "There was an error downloading the binaries, please try again."
+				return 1
+			fi
+		else
+			echo "Your binaries are up-to-date."
+			return 0
+		fi
+	fi
+}
+
 ### Cake Start
 cake_start() {
-	logger "Cake Queue Management Starting - settings: ${2} | ${3} | ${4}"
+	options=${4}
+	case "${options}" in 
+		*diffserv3*|*diffserv4*|*diffserv8*|*besteffort*)
+			# priority queue specified
+			;;
+		*)
+			# priority queue not specified, default to besteffort
+			options="besteffort ${options}"
+			;;
+	esac
+	logger "Cake Queue Management Starting - settings: ${2} | ${3} | ${options}"
 	runner disable 2>/dev/null
 	fc disable 2>/dev/null
 	fc flush 2>/dev/null
 	insmod /opt/lib/modules/sch_cake.ko 2>/dev/null
-	/opt/sbin/tc qdisc replace dev eth0 root cake bandwidth "${3}" besteffort nat ${4}
+	/opt/sbin/tc qdisc replace dev eth0 root cake bandwidth "${3}" nat ${options}
 	ip link add name ifb9eth0 type ifb
 	/opt/sbin/tc qdisc del dev eth0 ingress 2>/dev/null
 	/opt/sbin/tc qdisc add dev eth0 handle ffff: ingress
 	/opt/sbin/tc qdisc del dev ifb9eth0 root 2>/dev/null
-	/opt/sbin/tc qdisc add dev ifb9eth0 root cake bandwidth "${2}" besteffort nat wash ingress ${4}
+	/opt/sbin/tc qdisc add dev ifb9eth0 root cake bandwidth "${2}" nat wash ingress ${options}
 	ifconfig ifb9eth0 up
 	/opt/sbin/tc filter add dev eth0 parent ffff: protocol all prio 10 u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ifb9eth0
 }
@@ -33,6 +91,23 @@ cake_stop() {
 	rmmod sch_cake 2>/dev/null
 	fc enable
 	runner enable
+}
+
+### Cake Disable
+cake_disable() {
+	logger "Cake Queue Management Disabled"
+	if [ -f /jffs/scripts/firewall-start ]; then
+		LINECOUNT=$(grep -c '# '"$SCRIPT_NAME" /jffs/scripts/firewall-start)
+		if [ "$LINECOUNT" -gt 0 ]; then
+			sed -i -e '/# '"$SCRIPT_NAME"'/d' /jffs/scripts/firewall-start
+		fi
+	fi
+	if [ -f /jffs/scripts/services-stop ]; then
+		LINECOUNT=$(grep -c '# '"$SCRIPT_NAME" /jffs/scripts/services-stop)
+		if [ "$LINECOUNT" -gt 0 ]; then
+			sed -i -e '/# '"$SCRIPT_NAME"'/d' /jffs/scripts/services-stop
+		fi
+	fi
 }
 
 ### Check Requirements
@@ -55,33 +130,23 @@ if [ "${1}" = "enable" ] || [ "${1}" = "start" ]; then
 		echo "Required parameters missing: $SCRIPT_NAME ${1} dlspeed upspeed \"optional extra parameters\""
 		echo ""
 		echo "Example #1: $SCRIPT_NAME ${1} 30Mbit 5000Kbit"
-		echo "Example #2: $SCRIPT_NAME ${1} 30Mbit 5Mbit \"docsis ack-filter\""
+		echo "Example #2: $SCRIPT_NAME ${1} 30Mbit 5Mbit \"diffserv4 docsis ack-filter\""
 		return 1
 	fi	
 fi
-if [ "${1}" = "install" ] && [ -z "$2" ]; then
-	echo "Required model missing: $SCRIPT_NAME ${1} {ac86u|ax88u}"
-	echo ""
-	echo "Example #1: $SCRIPT_NAME ${1} ac86u"
-	echo "Example #2: $SCRIPT_NAME ${1} ax88u"
-	return 1
+if [ "${1}" = "install" ] || [ "${1}" = "update" ]; then
+	if [ -z "$2" ]; then
+		echo "Required model missing: $SCRIPT_NAME ${1} {ac86u|ax88u}"
+		echo ""
+		echo "Example #1: $SCRIPT_NAME ${1} ac86u"
+		echo "Example #2: $SCRIPT_NAME ${1} ax88u"
+		return 1
+	fi
 fi
 
 case $1 in
-	install)
-		if [ "${2}" = "ac86u" ]; then
-			FILE1="sched-cake-oot_2020-05-28-a5dccfd8-1_aarch64-3.10.ipk"
-		elif [ "${2}" = "ax88u" ]; then
-			FILE1="sched-cake-oot_2020-05-28-a5dccfd8-ax_aarch64-3.10.ipk"
-		fi
-		FILE2="tc-adv_4.16.0-git-20191110_aarch64-3.10.ipk"
-		/usr/sbin/curl --retry 3 "https://raw.githubusercontent.com/ttgapers/cakeqos-merlin/master/${FILE1}" -o "/tmp/home/root/${FILE1}"
-		/usr/sbin/curl --retry 3 "https://raw.githubusercontent.com/ttgapers/cakeqos-merlin/master/${FILE2}" -o "/tmp/home/root/${FILE2}"
-		/opt/bin/opkg install "/tmp/home/root/${FILE1}"
-		/opt/bin/opkg install "/tmp/home/root/${FILE2}"
-		rm "/tmp/home/root/${FILE1}"
-		rm "/tmp/home/root/${FILE2}"
-		return 0
+	install|update)
+		cake_download "${@}"
 		;;
 	enable)
 		# Start
@@ -120,25 +185,8 @@ case $1 in
 			echo "/jffs/scripts/$SCRIPT_NAME stop"' # '"$SCRIPT_NAME" >> /jffs/scripts/services-stop
 			chmod 0755 /jffs/scripts/services-stop
 		fi
-		logger "Cake Queue Management Enabled - settings: ${2} | ${3} | ${4}"
+		logger "Cake Queue Management Enabled"
 		cake_start "${@}"
-		return 0
-		;;
-	disable)
-		cake_stop
-		if [ -f /jffs/scripts/firewall-start ]; then
-			LINECOUNT=$(grep -c '# '"$SCRIPT_NAME" /jffs/scripts/firewall-start)
-			if [ "$LINECOUNT" -gt 0 ]; then
-				sed -i -e '/# '"$SCRIPT_NAME"'/d' /jffs/scripts/firewall-start
-			fi
-		fi
-		if [ -f /jffs/scripts/services-stop ]; then
-			LINECOUNT=$(grep -c '# '"$SCRIPT_NAME" /jffs/scripts/services-stop)
-			if [ "$LINECOUNT" -gt 0 ]; then
-				sed -i -e '/# '"$SCRIPT_NAME"'/d' /jffs/scripts/services-stop
-			fi
-		fi
-		logger "Cake Queue Management Disabled"
 		return 0
 		;;
 	start)
@@ -149,14 +197,29 @@ case $1 in
 		cake_stop
 		return 0
 		;;
+	disable)
+		cake_stop
+		cake_disable
+		return 0
+		;;
+	uninstall)
+		cake_stop
+		cake_disable
+		opkg --autoremove remove sched-cake-oot
+		opkg --autoremove remove tc-adv
+		rm /jffs/scripts/$SCRIPT_NAME
+		return 0
+		;;
 	*)
-		echo "Usage: $SCRIPT_NAME {install|enable|start|stop|disable} (install, enable, and start have required parameters)"
+		echo "Usage: $SCRIPT_NAME {install|update|enable|start|stop|disable|uninstall} (install, update, enable, and start have required parameters)"
 		echo ""
-		echo "install: download and install necessary $SCRIPT_NAME binaries"
-		echo "enable:  start $SCRIPT_NAME and add to startup"
-		echo "start:   start $SCRIPT_NAME"
-		echo "stop:    stop $SCRIPT_NAME"
-		echo "disable: stop $SCRIPT_NAME and remove from startup"
+		echo "install:   download and install necessary $SCRIPT_NAME binaries"
+		echo "update:    update $SCRIPT_NAME binaries (if any available)"
+		echo "enable:    start $SCRIPT_NAME and add to startup"
+		echo "start:     start $SCRIPT_NAME"
+		echo "stop:      stop $SCRIPT_NAME"
+		echo "disable:   stop $SCRIPT_NAME and remove from startup"
+		echo "uninstall: stop $SCRIPT_NAME, remove from startup, and remove cake binaries"
 		return 1
 		;;
 esac
