629cef7288fa85be6e0947e1b5c6a430ab34884c
jackiechun <jackiechun@dyu.ca>
2020-06-22 23:32:26 -0400

    Add newlines

--- cake-qos.sh
+++ cake-qos.sh
@@ -313,7 +313,7 @@ case $1 in
 				echo "/jffs/scripts/$SCRIPT_NAME start ${2} ${3} \"${4}\" &"' # '"$SCRIPT_NAME_FANCY" >> "$WDog"
 			fi
 		else
-			printf "#!/bin/sh\n\n/jffs/scripts/$SCRIPT_NAME start ${2} ${3} \"${4}\" & # $SCRIPT_NAME_FANCY" >> /jffs/scripts/nat-start
+			printf "#!/bin/sh\n\n/jffs/scripts/$SCRIPT_NAME start ${2} ${3} \"${4}\" & # $SCRIPT_NAME_FANCY\n" >> /jffs/scripts/nat-start
 			echo "/jffs/scripts/$SCRIPT_NAME start ${2} ${3} \"${4}\" &"' # '"$SCRIPT_NAME_FANCY" >> "$WDog"
 			chmod 0755 /jffs/scripts/nat-start
 		fi
@@ -331,7 +331,7 @@ case $1 in
 			fi
 		else
 			SCRIPT_NAME="cake-qos"
-			printf "#!/bin/sh\n\n/jffs/scripts/$SCRIPT_NAME stop # $SCRIPT_NAME_FANCY" >> /jffs/scripts/services-stop
+			printf "#!/bin/sh\n\n/jffs/scripts/$SCRIPT_NAME stop # $SCRIPT_NAME_FANCY\n" >> /jffs/scripts/services-stop
 			chmod 0755 /jffs/scripts/services-stop
 		fi
 		
