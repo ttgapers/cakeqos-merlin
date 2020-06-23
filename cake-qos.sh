5dd54076566bb57527439355661159ac07ecec2d
maghuro <mrasquinho@gmail.com>
2020-06-23 01:23:49 +0100

    go back to develop

--- cake-qos.sh
+++ cake-qos.sh
@@ -8,7 +8,7 @@
 readonly SCRIPT_VERSION="v0.0.6"
 readonly SCRIPT_NAME="cake-qos"
 readonly SCRIPT_NAME_FANCY="CakeQOS-Merlin"
-readonly SCRIPT_BRANCH="master"
+readonly SCRIPT_BRANCH="develop"
 WDogdir="/jffs/addons/$SCRIPT_NAME.d"
 WDog="${WDogdir}/cake-watchdog.sh"
 
